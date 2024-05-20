0c45ddb3f9b8b088b0e50ab791ece44a09f98623
Fernando Nicolás <he440185@uaeh.edu.mx>
2024-05-19 20:23:57 -0600

    Fix text size loading...

--- src/app/components/loading.tsx
+++ src/app/components/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Loading() {
   return (
     <div className='flex justify-center items-center'>
-      <p className='w-1/2 text-center mt-6 text-[#0069FF] 2xl:text-lg xl:text-xl lg:text-xl md:text-xl sm:text-xl text-sm'>
+      <p className='w-1/2 text-center mt-6 text-[#0069FF] 2xl:text-lg xl:text-xl lg:text-xl md:text-xl sm:text-xl text-base'>
         Cargando publicaciones
         <span className="dot font-bold">.</span>
         <span className="dot font-bold">.</span>
